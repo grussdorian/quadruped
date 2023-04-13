04eecceb460162c4d8beeb8c84ca75596918d481
grussdorian <hardikghoshal@gmail.com>
2023-04-13 21:46:40 +0530

    calibration and move to zero

--- servo_code/move_to_zero.py
+++ servo_code/move_to_zero.py
@@ -1,6 +1,6 @@
 from gpiozero import Servo
 from time import sleep
-
+import sys
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 factory = PiGPIOFactory()
@@ -21,11 +21,28 @@ J12 = 25
 MIN_PULSE_WIDTH = 500/1000000
 MAX_PULSE_WIDTH = 2500/1000000
 
-servo_J5 = Servo(J5, min_pulse_width=MIN_PULSE_WIDTH,
+servo = Servo(J1, min_pulse_width=MIN_PULSE_WIDTH,
                  max_pulse_width=MAX_PULSE_WIDTH, pin_factory=factory)
+# servo2 = Servo(J5, min_pulse_width=MIN_PULSE_WIDTH,
+#                  max_pulse_width=MAX_PULSE_WIDTH, pin_factory=factory)
+mode = sys.argv[1]
+if mode == "min":
+    # MIN
+    servo.min()
+    # servo2.max()
+    sleep(0.5)
+    
+if mode == "max":
+    # MIN
+    servo.max()
+    # servo2.min()
+    sleep(0.5)
 
-# MIN
-servo_J5.min()
-sleep(0.5)
+if mode == "mid":
+    # MID
+    servo.mid()
+    # servo2.mid()
+    sleep(0.5)
+
 print("Clean exit")
 factory.close()  # Prevents the servos from jumping to the previously written state immediately after powering on
